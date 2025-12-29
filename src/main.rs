f810c4ab874a5ae4a677c558bda3df661216059a
Tali Auster <me@tali.network>
2025-12-29 15:02:03 -0700

    refactor: use anyhow in main binary

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::env;
 use tdl::tidal::Access;
 
+use anyhow::{Context, Result};
+
 #[tokio::main]
 async fn main() {
-    let client_id = env::var("TDL_CLIENT_ID");
-    let client_secret = env::var("TDL_CLIENT_SECRET");
-    let streaming_tok = env::var("TDL_BEARER_STREAMING");
+    if let Err(e) = inner().await {
+        eprintln!("\x1b[31;1mA fatal error occurred:\x1b[0m {e:?}");
+        std::process::exit(1);
+    }
 
-    let (Ok(client_id), Ok(client_secret), Ok(streaming_tok)) =
-        (client_id, client_secret, streaming_tok)
-    else {
-        panic!("env vars");
-    };
+    async fn inner() -> Result<()> {
+        let client_id =
+            env::var("TDL_CLIENT_ID").with_context(|| "Failed to find $TDL_CLIENT_ID")?;
+        let client_secret =
+            env::var("TDL_CLIENT_SECRET").with_context(|| "Failed to find $TDL_CLIENT_SECRET")?;
+        let streaming_tok = env::var("TDL_BEARER_STREAMING")
+            .with_context(|| "Failed to find $TDL_BEARER_STREAMING")?;
 
-    let access = Access::log_in(&client_id, &client_secret, &streaming_tok)
-        .await
-        .expect("todo error stuff");
+        let access = Access::log_in(&client_id, &client_secret, &streaming_tok).await?;
+        access.download_track("441696040").await?;
 
-    access.download_track("441696040").await.expect("lol");
+        Ok(())
+    }
 }
